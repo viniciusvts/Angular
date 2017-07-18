b064da84260f4b9c22f3d3e68181e480d5277235
Vinicius de Santana <vinicius.vts@gmail.com>
2017-07-18 20:42:30 -0300

    diretivas ngif

--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -3,9 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-diretiva-ngif',
   templateUrl: './diretiva-ngif.component.html',
-  styleUrls: ['./diretiva-ngif.component.css']
+  styleUrls: ['./diretiva-ngif.component.scss']
 })
 export class DiretivaNgifComponent implements OnInit {
+
+  cursos:string[] = [];
+
+  mostrarCursos:boolean = false;
+
+  onMostrarCursos(){
+    this.mostrarCursos = !this.mostrarCursos;
+  }
 
   constructor() { }
 
