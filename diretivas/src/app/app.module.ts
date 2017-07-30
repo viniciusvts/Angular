ff9e3aa089e1dccac97a83691ca5d8a93d35e718
Vinicius de Santana <vinicius.vts@gmail.com>
2017-07-29 22:50:37 -0300

    ng-content

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
 import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
+import { NgContentComponent } from './ng-content/ng-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
     DiretivaNgforComponent,
-    DiretivaNgclassComponent
+    DiretivaNgclassComponent,
+    NgContentComponent
   ],
   imports: [
     BrowserModule
